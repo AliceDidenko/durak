94a2594349a55dcd87ba511b1068ad1757541d2e
Alice <alice.didenko.1998.a@mail.ru>
2020-08-03 16:10:49 +0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/components/MainDiv.js
+++ src/components/MainDiv.js
@@ -7,6 +7,7 @@ function MainDiv() {
     return (
         <div className="main-field">
             <FieldTop/>
+            <div>npm add jquery</div>
             <FieldBottom/>
         </div>
     )
